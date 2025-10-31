457b52e730e8afb7edde7195f070c76f9ac12b30
eriobi <sirima.kav@gmail.com>
2025-10-31 12:20:30 +0700

    ให้ส่งกลับเป็น obj

--- backend/controllers/studentManagement.js
+++ backend/controllers/studentManagement.js
@@ -1,12 +1,17 @@
 import pool from "../pool.js";
 
-export const getStudent = async (req , res) => {
-    try{
-        const[row] = await pool.execute('SELECT * FROM student')
-        const[is_active] = await pool.execute('SELECT is_active FROM user')
-        res.json(row,is_active)
+export const getStudent = async (req, res) => {
+    try {
+        const [rows] = await pool.execute(
 
-    }catch(err){
+            `SELECT s.*, u.is_active
+            FROM student s
+            JOIN users u ON s.user_id = u.user_id`
+        )
+
+        res.json({ students: rows })
+
+    } catch (err) {
         return res.status(500).json({ message: 'Server error' })
     }
 }
