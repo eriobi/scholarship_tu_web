d0e34b89f77b285a8ddb41f47b0257315e1f15cb
eriobi <sirima.kav@gmail.com>
2025-11-17 19:33:14 +0700

    เพิ่มทุน ข่าว calender

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,70 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../axiosInstance";
 
-import CSTU from '../assets/CSTU.png'
+import CSTU from "../assets/CSTU.png";
+import ScholarshipCard from "../components/ScholarshipCard";
+import NewsCard from "../components/NewsCard";
+import Calender from "../components/Calender";
 
-const Home = () => {
+function Home() {
+  const [scholarships, setScholarships] = useState([]);
+  const [bookmarks, setBookmarks] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axiosInstance.get("/api/scholarships");
+        setScholarships(res.data);
+
+        const resBookmarks = await axiosInstance.get("/api/bookmarks");
+        setBookmarks(resBookmarks.data.map((b) => b.scho_id));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  /* กด bookmark */
+  const handleBookmark = async (id) => {
+    await axiosInstance.post(`/api/scholarships/${id}/bookmark`);
+
+    const resBookmarks = await axiosInstance.get("/api/bookmarks");
+    setBookmarks(resBookmarks.data.map((b) => b.scho_id));
+
+    const resCards = await axiosInstance.get("/api/scholarships");
+    setScholarships(resCards.data);
+  };
+
+  /* กด สมัครรับทุน */
+  const handleEnroll = (id) => {
+    console.log("Enroll success:", id);
+  };
 
   return (
     <div>
-      <img src={CSTU} className='relative w-full min-w-[1100px] bg-center  bg-cover'  alt=''/>
-      <ul>
-        <li>ปฏิทิน</li>
-        <li>กระดานข่าว</li>
-      </ul>
+      <img
+        src={CSTU}
+        className="relative w-full min-w-[1100px] bg-center  bg-cover"
+        alt=""
+      />
+      <div>
+        {scholarships.map((s) => (
+          <ScholarshipCard
+            key={s.scholarship_id}
+            scholarship={s}
+            bookmarked={bookmarks.includes(s.scholarship_id)}
+            onBookmark={handleBookmark}
+            onEnroll={handleEnroll}
+          />
+        ))}
+      </div>
+      <span>
+        <Calender />
+        <NewsCard />
+      </span>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
