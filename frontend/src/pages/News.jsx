268929595c3b75e6c912a1512c8f86070341fb2b
eriobi <sirima.kav@gmail.com>
2025-11-17 19:34:33 +0700

    เพิ่ม ui

--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import NewsCard from '../components/NewsCard'
 
 const News = () => {
   return (
     <div>
-      <h1>news</h1>
+       <div className="mb-8">
+        <h2 className="text-lg font-semibold mb-2">ข่าวประชาสัมพันธ์</h2>
+        <NewsCard />
+      </div>
+
     </div>
   )
 }
