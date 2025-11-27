dce83e54aaf5db9fcb8c61b4b8cdd054f12e904c
eriobi <sirima.kav@gmail.com>
2025-11-27 22:55:22 +0700

    เปลี่ยน varchar 255 เป็น 500 ให้ token โดนadd

--- project_db.sql
+++ project_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2025 at 10:41 PM
+-- Generation Time: Nov 27, 2025 at 04:53 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -213,8 +213,8 @@ CREATE TABLE `scholarship_info` (
 --
 
 INSERT INTO `scholarship_info` (`scholarship_id`, `scho_name`, `scho_year`, `qualification`, `scho_type`, `scho_source`, `start_date`, `end_date`, `scho_desp`, `image_file`, `scho_file`, `is_active`, `created_at`, `updated_at`) VALUES
-(8, 'ทุน test', 2568, 5, 'ทุนระยะยาว', 'ทุนภายใน', '2025-11-13', '2025-11-26', 'test ', '1763109390034-CSTU.png', '1763108548048-Screenshot 2025-05-07 065504.pdf', 1, '2025-11-12 08:46:07', '2025-11-17 07:47:01'),
-(9, 'ทุนจำปา', 2568, 6, 'ทุนเหมาจ่าย', 'ทุนภายนอก', '2025-11-19', '2025-11-29', 'มีจิตอาสา', NULL, '', 1, '2025-11-12 10:23:00', '2025-11-12 10:23:00');
+(8, 'ทุน test', 2568, 5, 'ทุนระยะยาว', 'ทุนภายใน', '2025-11-13', '2025-11-26', 'test ', '1763109390034-CSTU.png', '1763108548048-Screenshot 2025-05-07 065504.pdf', 1, '2025-11-12 08:46:07', '2025-11-26 15:23:32'),
+(9, 'ทุนจำปา', 2568, 6, 'ทุนเหมาจ่าย', 'ทุนภายนอก', '2025-11-19', '2025-11-29', 'มีจิตอาสา', NULL, NULL, 1, '2025-11-12 10:23:00', '2025-11-26 15:23:00');
 
 -- --------------------------------------------------------
 
@@ -252,7 +252,7 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`std_id`, `user_id`, `std_name`, `std_lastname`, `std_year`, `std_gpa`, `std_income`, `created_at`, `updated_at`) VALUES
-(680741145, 2, 'กิติยาวี', 'ส่องแสง', 2, 3.25, 100000, '0000-00-00 00:00:00', '2025-11-26 04:19:26');
+(680741145, 2, 'กิติยาวี', 'ส่องแสง', 2, 3.25, 100000, '0000-00-00 00:00:00', '2025-11-26 04:58:14');
 
 -- --------------------------------------------------------
 
@@ -279,7 +279,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `role`, `email`, `password`, `decryption`, `line_id`, `phone`, `is_active`, `created_at`, `updated_at`) VALUES
 (1, 'admin', 'sirima.kav@gmail.com', '$2b$10$Nbtr29E0.o94jbgjvn01Y.rzkFg0K.s9DkRAI/nVCkXdOModJXD.m', '', '', NULL, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 'student', 'daw@gmail.com', '$2b$10$hVcmpvMHF5638HHqPLX.KuFhayc3h.J3sYQQhB./iqukTSbmWYSNW', '', '', '0999999', 1, '0000-00-00 00:00:00', '2025-11-26 04:37:19');
+(2, 'student', 'daw@gmail.com', '$2b$10$hVcmpvMHF5638HHqPLX.KuFhayc3h.J3sYQQhB./iqukTSbmWYSNW', '', '', '0944444', 1, '0000-00-00 00:00:00', '2025-11-26 05:10:47');
 
 -- --------------------------------------------------------
 
@@ -290,50 +290,11 @@ INSERT INTO `users` (`user_id`, `role`, `email`, `password`, `decryption`, `line
 CREATE TABLE `users_session` (
   `session_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `token` varchar(255) NOT NULL,
+  `token` varchar(500) NOT NULL,
   `is_active` tinyint(1) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users_session`
---
-
-INSERT INTO `users_session` (`session_id`, `user_id`, `token`, `is_active`, `created_at`, `updated_at`) VALUES
-(11, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYwNDkxOTg4LCJleHAiOjE3NjA1MDI3ODh9.kCm8IYVxZitR5_1eDw_XSQuWT8CaACD0FZAJev6lIeY', 1, '2025-10-15 08:33:08', '2025-10-15 01:33:08'),
-(12, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYwNDk0MDY3LCJleHAiOjE3NjA1MDQ4Njd9.LlSpl4DH_4vpYxbciROq11w5H0-xVXf0JuiZ3fiEYaI', 1, '2025-10-15 09:07:47', '2025-10-15 02:07:47'),
-(13, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYwNTgyMDg1LCJleHAiOjE3NjA1OTI4ODV9.oOJ5UAxStw_6mCWI2hFUjs9SSTo6nsM8Cy6suuZAlRI', 1, '2025-10-16 09:34:45', '2025-10-16 02:34:45'),
-(14, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYwNzQ2ODQwLCJleHAiOjE3NjA3NTc2NDB9.M1Ud0_stnZySxy0eSSMpDtFfu1xjDQjJEc0fN1HhOTs', 1, '2025-10-18 07:20:40', '2025-10-18 00:20:40'),
-(15, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYyMTU1NDU5LCJleHAiOjE3NjIxNjYyNTl9._IOGuMeEZ8w2CAd_qNL3ic6rb8QhshmmDmgxmwC5oj4', 1, '2025-11-03 14:37:39', '2025-11-03 07:37:39'),
-(16, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYyMjk4Njc1LCJleHAiOjE3NjIzMDIyNzV9.ZcH9vlOi4foB7oAQbk9qc4I_DklsTLTt9DA67PLNeYM', 1, '2025-11-05 06:24:35', '2025-11-04 23:24:35'),
-(17, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjIzMDE4NjAsImV4cCI6MTc2MjMwNTQ2MH0.bV6QtB60P2ATLBaNWGQnowyUgtw-ZE2a62VdZj4ke1s', 1, '2025-11-05 07:17:40', '2025-11-05 00:17:40'),
-(18, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYyMzA1MTkxLCJleHAiOjE3NjIzMDg3OTF9.Fr1ktxLZNaF3tfQnVFSY3kjLayZy_x6PPHHEYjrjgbY', 1, '2025-11-05 08:13:11', '2025-11-05 01:13:11'),
-(19, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjIzMDUyMTUsImV4cCI6MTc2MjMwODgxNX0.CI9mSX9cBrUXh_PDHVlECYopHlOVQFhaiFJNojsIN9c', 1, '2025-11-05 08:13:35', '2025-11-05 01:13:35'),
-(20, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYyOTAxNDkxLCJleHAiOjE3NjI5MDUwOTF9.m2EV6kRZ_LZSR4MBgQwZU1dS0WVX9OBLEGza2Orf-XQ', 1, '2025-11-12 05:51:31', '2025-11-11 22:51:31'),
-(21, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjI5MTc3OTksImV4cCI6MTc2MjkyMTM5OX0.SSdMiapZhlskfdf2cbRO9lP8HpsVl63No-0AWq3r-Xc', 1, '2025-11-12 10:23:19', '2025-11-12 03:23:19'),
-(22, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYyOTE4MDk3LCJleHAiOjE3NjI5MjE2OTd9.IJBWkEfbVANeDPB28CAI8jANU0mL9lAh7ceJ8qhJWWc', 1, '2025-11-12 10:28:17', '2025-11-12 03:28:17'),
-(23, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjI5MTgxOTUsImV4cCI6MTc2MjkyMTc5NX0.1xfGvlvtrdxSoeaGdtOYZcrZfidhqhAJX9kzMbwai9E', 1, '2025-11-12 10:29:55', '2025-11-12 03:29:55'),
-(24, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjMwMDU1MzMsImV4cCI6MTc2MzAwOTEzM30.hojblcJZ01Y-F685zz5pJlXluyLnPGzE5LBI7ZSYJeY', 1, '2025-11-13 10:45:33', '2025-11-13 03:45:33'),
-(25, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjMwMDU1NDQsImV4cCI6MTc2MzAwOTE0NH0.rSjTnzb_3B-aZaj8uwWh7k5fE7YgsIQTigU2Ofjj3Z8', 1, '2025-11-13 10:45:44', '2025-11-13 03:45:44'),
-(26, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjMwMDU2OTIsImV4cCI6MTc2MzAwOTI5Mn0.jtPgcVBXUEQj_BxG_uwP04r-x_SpmE_o1Q9uerylpbU', 1, '2025-11-13 10:48:12', '2025-11-13 03:48:12'),
-(27, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjMwMDU5NzUsImV4cCI6MTc2MzAwOTU3NX0.FxL5OjSelS5jTRMXH8_D7eEudhPbjF3HXRLn9epLkhY', 1, '2025-11-13 10:52:55', '2025-11-13 03:52:55'),
-(28, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjMwMDYxMDgsImV4cCI6MTc2MzAwOTcwOH0.j1Qby9G5ZOrj2b30v2cMg1VirtXKwY3XsPWN1Eb_7_4', 1, '2025-11-13 10:55:08', '2025-11-13 03:55:08'),
-(29, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjMwMDYxNDUsImV4cCI6MTc2MzAwOTc0NX0.UaoH_RCzIZDfrgJvz23KrwsWVN_9qVaQkYuE3fI9JPc', 1, '2025-11-13 10:55:45', '2025-11-13 03:55:45'),
-(30, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYzMDA2MTY0LCJleHAiOjE3NjMwMDk3NjR9.YJ2qoVorjysKXMaPhLztKh3oAm8Gp5ZmiNSfiLsMgTo', 1, '2025-11-13 10:56:04', '2025-11-13 03:56:04'),
-(31, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjMwMjQ4MDUsImV4cCI6MTc2MzYyOTYwNX0.m7oQRenXruTrZONivjPd100V6dhJP750fVZSpZXKL94', 1, '2025-11-13 16:06:45', '2025-11-13 09:06:45'),
-(32, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYzMDkxMTkyLCJleHAiOjE3NjM2OTU5OTJ9.JekFtIM2RAqKfVwfkaCUPoflcs5HQSHNl8QLIreGpyI', 1, '2025-11-14 10:33:12', '2025-11-14 03:33:12'),
-(33, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjMxMTg4MjUsImV4cCI6MTc2MzcyMzYyNX0.xkCe0EJPGKJ6JoopQ9VOLTT-eZwYmfI0SaaJCZLGXuM', 1, '2025-11-14 18:13:45', '2025-11-14 11:13:45'),
-(34, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjMxMjA3NTUsImV4cCI6MTc2MzcyNTU1NX0.Uactnro7RJIdN1m87ZeB1LpR0GjHzaZlMfsbObPRZLI', 1, '2025-11-14 18:45:55', '2025-11-14 11:45:55'),
-(35, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjMzNTk3ODgsImV4cCI6MTc2Mzk2NDU4OH0.xh9Xm7nnKU1IFxEPbJDTbHYPn-69r3eR8KoPY93Uas4', 1, '2025-11-17 13:09:48', '2025-11-17 06:09:48'),
-(36, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InNpcmltYS5rYXZAZ21haWwuY29tIiwiaWF0IjoxNzYzNzIwNzcyLCJleHAiOjE3NjQzMjU1NzJ9.mc9Oa6SDE-dlGKegBO2-5Dbwugb_AvdIGiTlJ-7G4VY', 1, '2025-11-21 17:26:12', '2025-11-21 10:26:12'),
-(37, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjM5NzExNzEsImV4cCI6MTc2NDU3NTk3MX0.tiSDOVGsCTK6X8Cl9fBq-KoATgWTipqz3FVFdOe8WGM', 1, '2025-11-24 14:59:31', '2025-11-24 07:59:31'),
-(38, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjM5Nzg2NDYsImV4cCI6MTc2NDU4MzQ0Nn0.Xs2lNly1MIMR_Ed6xckpd6ccp0EEbljYciIlLIh42QA', 1, '2025-11-24 17:04:06', '2025-11-24 10:04:06'),
-(39, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjM5ODE3NDMsImV4cCI6MTc2NDU4NjU0M30.MKcbApLvepR3ywBN0XQl-jxZD9lVf6LJDJHEsnoPdq4', 1, '2025-11-24 17:55:43', '2025-11-24 10:55:43'),
-(40, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjM5ODE3ODIsImV4cCI6MTc2NDU4NjU4Mn0.lYLa3asT_Wzae_DItJ_5_zh_S22GE0VTBrI3I2XntkA', 1, '2025-11-24 17:56:22', '2025-11-24 10:56:22'),
-(41, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjM5ODIzNzAsImV4cCI6MTc2NDU4NzE3MH0.IAcgmR_eXcxPF-dxwmivAORoK3ezBDLTg25z9m-QGAg', 1, '2025-11-24 18:06:10', '2025-11-24 11:06:10'),
-(42, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjM5ODI2ODAsImV4cCI6MTc2NDU4NzQ4MH0.kLCeY6OBH-cU0JTH-4uXOjcvXMgLd_aDbOtwkHjugpQ', 1, '2025-11-24 18:11:20', '2025-11-24 11:11:20'),
-(43, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRhd0BnbWFpbC5jb20iLCJpYXQiOjE3NjQwOTcyMzAsImV4cCI6MTc2NDcwMjAzMH0.nECesTS4YD6-0fGR_dCn1oJGzf23E6v1jw3K8DpHno4', 1, '2025-11-26 02:00:30', '2025-11-25 19:00:30');
 
 --
 -- Indexes for dumped tables
@@ -511,7 +472,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users_session`
 --
 ALTER TABLE `users_session`
-  MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- Constraints for dumped tables
